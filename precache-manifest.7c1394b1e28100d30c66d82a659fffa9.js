4ddf856c0fb127f64fe13ce7504265fb16e67bc3
Luka Pršina <luk.prsina@gmail.com>
2020-10-12 20:02:52 +0200

    Updates

---
+++ precache-manifest.7c1394b1e28100d30c66d82a659fffa9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "681a29e61884c28823769594e503278c",
+    "url": "/novice/index.html"
+  },
+  {
+    "revision": "df5ac9538970251fac07",
+    "url": "/novice/static/css/2.7ec11378.chunk.css"
+  },
+  {
+    "revision": "619bd1edac225cf093c4",
+    "url": "/novice/static/css/main.008ad3a7.chunk.css"
+  },
+  {
+    "revision": "df5ac9538970251fac07",
+    "url": "/novice/static/js/2.06f17140.chunk.js"
+  },
+  {
+    "revision": "cac7bd24932edf483192b84a0a289373",
+    "url": "/novice/static/js/2.06f17140.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "619bd1edac225cf093c4",
+    "url": "/novice/static/js/main.f05f3fde.chunk.js"
+  },
+  {
+    "revision": "3d7ec9381d2c889cb889",
+    "url": "/novice/static/js/runtime-main.1258c7e9.js"
+  },
+  {
+    "revision": "062705995798aff1305de83bf13bfa7b",
+    "url": "/novice/static/media/arrow.06270599.svg"
+  },
+  {
+    "revision": "72dc2fca1d4f867c88687aee656f1fbb",
+    "url": "/novice/static/media/wood.72dc2fca.jpg"
+  }
+]);
